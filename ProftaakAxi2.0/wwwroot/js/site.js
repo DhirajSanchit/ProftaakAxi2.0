ae8c6f8b57ffb9782fef87b28f63f53225197c17
Tom <tom.beusch@stud.fh-campuswien.ac.at>
2022-03-16 11:38:11 +0100

    block hide button added

--- ProftaakAxi2.0/wwwroot/js/site.js
+++ ProftaakAxi2.0/wwwroot/js/site.js
@@ -1,4 +1,10 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+function hideElement(elementId){
+    document.getElementById(elementId).style.display = "none"
+}
 
+function showElement(elementId){
+    document.getElementById(elementId).style.display = "block"
+}
 // Write your JavaScript code.
