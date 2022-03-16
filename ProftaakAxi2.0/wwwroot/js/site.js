2f4fa17502640b6e957dcb02990355fdaec18cc5
Cage03 <timmermansstijn1@gmail.com>
2022-03-16 12:09:11 +0100

    Added buttons to hide and unhide new-article-modal

--- ProftaakAxi2.0/wwwroot/js/site.js
+++ ProftaakAxi2.0/wwwroot/js/site.js
@@ -1,9 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+
 function hideElement(elementId){
     document.getElementById(elementId).style.display = "none"
 }
-
 function showElement(elementId){
     document.getElementById(elementId).style.display = "block"
 }
